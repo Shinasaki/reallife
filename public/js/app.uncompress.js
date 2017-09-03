f7f1a07aea56a7e412a5b55667fcba94a6466e81
Shinasaki <Shinasaki07@gmail.com>
2017-09-03 14:18:18 +0700

    (Public) Edit

--- public/js/app.uncompress.js
+++ public/js/app.uncompress.js
@@ -17,6 +17,45 @@ $(function() {
         }, 800, 'easeInQuad');
     }, 800);
 
+
+    // Keydown
+    $(document).keydown(function(event) {
+        // Disable F12
+        if (event.keyCode == 123) {
+            return false;
+        }
+        else if (event.ctrlKey && event.shiftKey && event.keyCode==73) {
+            return false;
+        }
+        // -->
+    });
+    // -->
+
+    // Disable Click Select
+    function disableselect(e){
+    return false
+    }
+    function reEnable(){
+    return true
+    }
+    document.onselectstart=new Function ("return false")
+    if (window.sidebar){
+    document.onmousedown=disableselect
+    document.onclick=reEnable
+    }
+    // -->
+
+
+    // Disable Right Click
+    function clickIE() {if (document.all) {(message);return false;}}
+    function clickNS(e) {if
+    (document.layers||(document.getElementById&&!document.all)) {
+    if (e.which==2||e.which==3) {(message);return false;}}}
+    if (document.layers)
+    {document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+    else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+    document.oncontextmenu=new Function("return false")
+    // -->
 });
 
 function audioOff(event) {
